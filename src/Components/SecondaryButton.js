ab6974089f8542234c3d56cf05888a7ecbafba96
axemsi <130647373+axemsi@users.noreply.github.com>
2023-04-19 01:44:43 +0000

    3/3

--- src/Components/SecondaryButton.js
+++ src/Components/SecondaryButton.js
@@ -9,7 +9,7 @@ function SecondaryButton(props) {
         border-radius: 24px;
         border: 1px solid #64748B;
         min-width: ${minWidth};
-        color: #152842;
+        color: #64748B;
         &:hover {
             background: #FFFFFF;
             border: 1px solid #FFFFFF;
--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,9 +3,9 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-4 mt-5'>
-            <div className='title'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-4'>
+        <div className='col px-4 d-inline-flex flex-column gap-4 border-adresses'>
+            <div className='title pt-4'>Адреса заведений</div>
+            <div className='text-rg d-inline-flex flex-column gap-4'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
